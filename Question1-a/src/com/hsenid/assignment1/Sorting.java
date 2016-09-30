4d1a23383f152866b7e680c4b8c4866f7ce128b4
Damitha <damitha@hsenidlanka.com>
2016-09-30 11:40:43 +0530

    Assignment1_Question1_a

---
+++ Question1-a/src/com/hsenid/assignment1/Sorting.java
@@ -0,0 +1,120 @@
+package com.hsenid.assignment1;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by hsenid on 9/30/16.
+ */
+public class Sorting {
+    public static void main(String[] args) {
+
+
+        Sorting();
+    }
+
+    /**
+     * Sorting()
+     * <p>
+     * In here we can get the Key board inputs and make them Ascending Order or Descending Order
+     * User can see the Ascending Order and  Descending Order individually
+     * User have to give key board input by giving the enter and line by line
+     */
+    private static void Sorting() {
+        for (int p = 0; ; p++) {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter How Many Inputs: ");
+            int myint = keyboard.nextInt();
+
+            int arr[] = new int[myint];
+
+            for (int j = 0; j < myint; j++) {
+                System.out.println("Enter Value " + ":");
+                arr[j] = keyboard.nextInt();
+            }
+            Scanner keyboard3 = new Scanner(System.in);
+            System.out.println("Output in horizontal Enter 1 or Output in vertical Enter 0 ");
+            int Pattern = keyboard3.nextInt();
+
+            Scanner keyboard2 = new Scanner(System.in);
+            System.out.println("Ascending Order Enter 3 or Descending Order Enter 2: ");
+            int virsion = keyboard2.nextInt();
+
+            if (Pattern == 1) {
+                if (virsion == 3) {
+                    System.out.println("Numbers in Ascending Order:");
+
+                    for (int k = 0; k < arr.length; k++) {
+                        if (k != arr.length - 1) {
+                            Arrays.sort(arr);
+                            System.out.print(" " + arr[k] + ",");
+                        } else {
+                            Arrays.sort(arr);
+                            System.out.print(" " + arr[k]);
+                        }
+                    }
+                } else if (virsion == 2) {
+                    System.out.println();
+                    Arrays.sort(arr);
+                    System.out.println("Numbers in Descending Order: ");
+
+                    for (int i = arr.length - 1; i >= 0; i--) {
+                        if (i != 0)
+                            System.out.print(" " + arr[i] + ",");
+                        else
+                            System.out.print(" " + arr[i]);
+                    }
+                    System.out.println(" ");
+                } else {
+                    System.out.println("Invalid Input Try again");
+                }
+            }
+            if (Pattern == 0) {
+                if (virsion == 3) {
+                    System.out.println("Numbers in Ascending Order:");
+
+                    for (int k = 0; k < arr.length; k++) {
+                        if (k != arr.length - 1) {
+                            Arrays.sort(arr);
+                            System.out.print(" " + arr[k] + ",");
+                        } else {
+                            Arrays.sort(arr);
+                            System.out.print(" " + arr[k]);
+                        }
+                    }
+                } else if (virsion == 2) {
+                    System.out.println();
+                    Arrays.sort(arr);
+                    System.out.println("Numbers in Descending Order: ");
+
+                    for (int i = arr.length - 1; i >= 0; i--) {
+                        if (i != 0)
+                            System.out.print(" " + arr[i] + ",");
+                        else
+                            System.out.print(" " + arr[i]);
+                    }
+                    System.out.println(" ");
+                } else {
+                    System.out.println("Invalid Input Try again");
+                }
+            }
+
+            System.out.println();
+            Scanner endCommand = new Scanner(System.in);
+            System.out.println("Do you want Continue ? (Y,N)");
+            String command = endCommand.nextLine();
+            if (command.equals("N")) {
+                break;
+            } else if (command.equals("Y"))
+                continue;
+            else
+                System.out.println("Not a valid command, please type Y or N." + command);
+        }
+
+    }
+}
+
+
+
+
+
+
